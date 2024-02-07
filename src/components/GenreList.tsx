e8182e5dfa6debc93361c064308f70c42250fd27
Connor MacDonell <connor.macdonell1@gmail.com>
2024-02-07 11:18:22 +0900

    Make generic hook for fetching data.

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,12 @@
-import React from "react";
 import useGenres from "../hooks/UseGenres";
 import { VStack } from "@chakra-ui/react";
-import GenreCard from "./GenreCard";
 
 const GenreList = () => {
-  const { genres, error, isLoading } = useGenres();
+  const { data, error, isLoading } = useGenres();
 
   return (
     <VStack>
-      {genres.map((genre) => (
+      {data.map((genre) => (
         <li key={genre.id}>{genre.name}</li>
       ))}
     </VStack>
--- src/hooks/UseGenres.ts
+++ src/hooks/UseGenres.ts
@@ -1,11 +1,5 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
-export interface GenreResponse {
-  count: number,
-  results: Genre[]
-}
 
 export interface Genre {
   id: number,
@@ -15,29 +9,6 @@ export interface Genre {
   image_background: string
 }
 
-const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<GenreResponse>("/genres",  { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message)
-      })
-      return () => controller.abort();
-    } ,[]);
-
-    return { genres, error, isLoading };
-};
+const useGenres = () => useData<Genre>('/genres')
 
 export default useGenres;
