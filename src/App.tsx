fd4f9a35fa6f75e69dd922d66f42f7822f4765fe
Connor MacDonell <connor.macdonell1@gmail.com>
2024-02-08 21:45:03 +0900

    Fix warning from chakra HStack.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -43,20 +43,22 @@ function App() {
           </GridItem>
         </Show>
         <GridItem area="main">
-          <HStack paddingLeft={10} spacing={2} marginBottom={1}>
-            <PlatformSelector
-              selectedPlatform={gameQuery.platform}
-              onSelectPlatform={(platform) => {
-                setGameQuery({ ...gameQuery, platform });
-              }}
-            ></PlatformSelector>
+          <Flex paddingLeft={10} marginBottom={1}>
+            <Box marginRight={5}>
+              <PlatformSelector
+                selectedPlatform={gameQuery.platform}
+                onSelectPlatform={(platform) => {
+                  setGameQuery({ ...gameQuery, platform });
+                }}
+              ></PlatformSelector>
+            </Box>
             <SortSelector
               selectedSortOrder={gameQuery.sortOrder}
               onSelectSortOrder={(sortOrder) => {
                 setGameQuery({ ...gameQuery, sortOrder });
               }}
             ></SortSelector>
-          </HStack>
+          </Flex>
           <GameGrid gameQuery={gameQuery}></GameGrid>
         </GridItem>
       </Grid>
