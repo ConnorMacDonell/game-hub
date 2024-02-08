856bebb721d35c6777df7eb305105cd9f9d286e4
Connor MacDonell <connor.macdonell1@gmail.com>
2024-02-08 22:32:57 +0900

    Add dynamic page heading.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Genre } from "./hooks/UseGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/usePlatforms";
 import SortSelector, { SortOrder } from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -48,22 +49,25 @@ function App() {
           </GridItem>
         </Show>
         <GridItem area="main">
-          <Flex paddingLeft={10} marginBottom={1}>
-            <Box marginRight={5}>
-              <PlatformSelector
-                selectedPlatform={gameQuery.platform}
-                onSelectPlatform={(platform) => {
-                  setGameQuery({ ...gameQuery, platform });
+          <Box paddingLeft={10}>
+            <GameHeading gameQuery={gameQuery}></GameHeading>
+            <Flex marginBottom={1}>
+              <Box marginRight={5}>
+                <PlatformSelector
+                  selectedPlatform={gameQuery.platform}
+                  onSelectPlatform={(platform) => {
+                    setGameQuery({ ...gameQuery, platform });
+                  }}
+                ></PlatformSelector>
+              </Box>
+              <SortSelector
+                selectedSortOrder={gameQuery.sortOrder}
+                onSelectSortOrder={(sortOrder) => {
+                  setGameQuery({ ...gameQuery, sortOrder });
                 }}
-              ></PlatformSelector>
-            </Box>
-            <SortSelector
-              selectedSortOrder={gameQuery.sortOrder}
-              onSelectSortOrder={(sortOrder) => {
-                setGameQuery({ ...gameQuery, sortOrder });
-              }}
-            ></SortSelector>
-          </Flex>
+              ></SortSelector>
+            </Flex>
+          </Box>
           <GameGrid gameQuery={gameQuery}></GameGrid>
         </GridItem>
       </Grid>
