e269796b30aa172bdadb9e246932916016bc0f9b
Connor MacDonell <connor.macdonell1@gmail.com>
2024-03-28 15:01:34 -0700

    Setup routing.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
 import PlatformIconList from "./PlatformIconList";
 import { Game } from "../services/gameService";
+import { Link } from "react-router-dom";
 
 interface Props {
   game: Game;
@@ -10,18 +11,22 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <HStack justifyContent={"space-between"} marginBottom={3}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          ></PlatformIconList>
-          <CriticScore score={game.metacritic}></CriticScore>
-        </HStack>
-        <Heading fontSize="2xl">{game.name}</Heading>
-      </CardBody>
-    </Card>
+    <>
+      <Link to="/games/${game.id}">
+        <Card>
+          <Image src={getCroppedImageUrl(game.background_image)} />
+          <CardBody>
+            <HStack justifyContent={"space-between"} marginBottom={3}>
+              <PlatformIconList
+                platforms={game.parent_platforms.map((p) => p.platform)}
+              ></PlatformIconList>
+              <CriticScore score={game.metacritic}></CriticScore>
+            </HStack>
+            <Heading fontSize="2xl">{game.name}</Heading>
+          </CardBody>
+        </Card>
+      </Link>
+    </>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
 import theme from "./theme";
 import "./index.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +17,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         initialColorMode={theme.config.initialColorMode}
       ></ColorModeScript>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
